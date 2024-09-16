37a25a117e44d13dc2ca6c3ceaecffc69c566395
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2024-09-16 22:31:09 +0700

    Add images and button

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -5,7 +5,51 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: GradientContainer());
+    return Scaffold(
+      body: Stack(
+        children: [
+          GradientContainer(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/dice-2.png'),
+                      width: 140,
+                      height: 140,
+                    ),
+                    const SizedBox(width: 20),
+                    Image(
+                      image: AssetImage('assets/images/dice-3.png'),
+                      width: 140,
+                      height: 140,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow[100],
+                    foregroundColor: Colors.grey[900],
+                    shadowColor: Colors.black.withOpacity(0.5),
+                    elevation: 5,
+                    minimumSize: const Size(150, 50),
+                  ),
+                  child: const Text(
+                    "Roll the Dice",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -23,15 +67,6 @@ class GradientContainer extends StatelessWidget {
           ],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 28,
-          ),
         ),
       ),
     );
